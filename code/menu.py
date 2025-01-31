6222ef42c1d1e6e06992670777c10269b6d55c08
Marcello Fiordiluglio <marcellofiordiluglio@gmail.com>
2025-01-31 14:52:15 -0300

    adding classes generated from StarUML

---
+++ code/menu.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Level import Level
+
+
+class Menu(Level):
+    def __init__(self):
+        self.Attribute1 = None
+        self.window = None
+
+    def run(self, ):
+        pass
