51f83014ada990c05e314a0dc02bfa29f58886fb
Marcello Fiordiluglio <marcellofiordiluglio@gmail.com>
2025-02-05 10:03:25 -0300

    creating Aasset folder Creating Menu player, Score, Exit

--- code/Const.py
+++ code/Const.py
@@ -1,3 +1,12 @@
 
+#C
+COLOR_BLACK = (0, 0, 0)
+COLOR_GREEN = (71, 254, 255)
+# M
+
+MENU_OPTION = ('DEMO NEW PLAYER 1',
+               'SCORE',
+               'EXIT')
+# W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
